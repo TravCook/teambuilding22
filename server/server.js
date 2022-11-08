f1db51d0d4419539e9867a166fe5c4b63b5f91f9
Travis Cook <tcook@instructors.2u.com>
2022-11-08 12:07:46 -0700

    fixing public path

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 3001;
 // Set up body parsing, static, and route middleware
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, '../client/build')));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 app.get('/remoteEntry.js', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/remoteEntry.js'));
   });
