bc8306674fd88b962108f1e3699fdf9f22850895
Travis Cook <tcook@instructors.2u.com>
2022-11-08 10:50:31 -0700

    changing build script

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -33,6 +33,7 @@ module.exports = {
             use: {
               loader: "babel-loader",
             },
+            options: { presets: ['@babel/env','@babel/preset-react'] },
           },
         ],
       },
