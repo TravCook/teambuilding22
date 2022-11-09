213dc53431d4da293642c5e4dcde38dc1b1f736c
Travis Cook <tcook@instructors.2u.com>
2022-11-09 10:59:54 -0700

    moving functionality onto remote

--- client/src/components/navbar/navbar.js
+++ client/src/components/navbar/navbar.js
@@ -38,7 +38,7 @@ const NavbarEl = (props) => {
   }
     return(
         <Navbar className="navbarMain" >
-                <Col lg={2} className="appName">{props.codeHubProps ? props.codeHubProps.name : "Teambuilder22"}</Col>
+                <Col lg={2} className="appName">{props.propsFromMain ? props.propsFromMain?.name : "Teambuilder22"}</Col>
                 <Col lg={5}>
                     <Row>Augmented Players:
                     {augmentPlayers()}
